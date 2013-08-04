abbf9642a7a655032fc54a00d7a8058b0e3fc812
Joaq <joaquin.almirante@gmail.com>
2013-08-04 11:56:18 +0800

    Comments

--- static/dashboard.js
+++ static/dashboard.js
@@ -1,6 +1,9 @@
 $(function() {
   $.get('http://localhost:5000/card', function(data) {
-    $("#test").html(data);
+
+
+    $("#test").html(data); //render template data
+    //create cards + link with ahref link
   });
   return false;
 });
